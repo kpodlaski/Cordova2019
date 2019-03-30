3ec4b06590d0b7f08885d6f8861ec08835019644
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-03-30 15:04:58 +0100

    openweathermap base2

--- WebWeatherApp/weather.js
+++ WebWeatherApp/weather.js
@@ -8,7 +8,16 @@ $.ajax({
     cache: false
     })
 .done(function (resp) {
-    alert(JSON.stringify(resp));
+    //alert(JSON.stringify(resp));
+    console.log(resp);
+    //alert(resp.main.temp+ " "
+    //      +resp.main.humidity);
+    var tempSpan = document.getElementById("temp");
+    tempSpan.innerText=(resp.main.temp-273);
+    var humSpan = document.getElementById("wilgotnosc");
+    humSpan.innerText = resp.main.humidity;
+    var iconImg = document.getElementById("icon");
+    iconImg.src="http://openweathermap.org/img/w/"+resp.weather[0].icon+".png"
 })
 .fail(function (msg){
   alert(JSON.stringify(msg));
