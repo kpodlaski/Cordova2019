d991c120e3fc1473e20dd4a0553d60485425b228
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-03-30 14:37:57 +0100

    ajax basic get

--- WebWeatherApp/weather.js
+++ WebWeatherApp/weather.js
@@ -1,2 +1,15 @@
 //http://api.openweathermap.org/data/2.5/weather?q=Lodz,pl&appid=64c7b98c91f425ed4bd193334c0bd9d6
 //ba4e5e220d6ab917912bc97d8e967ba1
+
+$.ajax({
+    method : "GET",
+    dataType: 'json',
+    url: "http://api.openweathermap.org/data/2.5/weather?q=Lodz,pl&appid=64c7b98c91f425ed4bd193334c0bd9d6",
+    cache: false
+    })
+.done(function (resp) {
+    alert(JSON.stringify(resp));
+})
+.fail(function (msg){
+  alert(JSON.stringify(msg));
+  });
