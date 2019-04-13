9b9ae72b339096fb4e4276fa20ab8619b421717f
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2019-04-13 11:19:14 +0200

    animation 2 balls

--- AnimationApp/www/js/paint.js
+++ AnimationApp/www/js/paint.js
@@ -1,51 +1,55 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
-var ball ={
-    img : new Image(), // note that the offscreen image must be declared OUTSIDE of the window.onload() function 
-    x: 10, 
-    y: 10,
-    vx :3,
-    vy :4,
+class Ball {
+    img = new Image();
+    vx =3;
+    vy =4;
 
-    init : function(canvas){
+    constructor(canvas, x, y){
         this.img.src = "img/ball.png";
         this.canvas = canvas;
-    },
+        this.x = x;
+        this.y = y;
+    };
 
-    move : function(){
+    move(){
         this.x += this.vx;
         this.y += this.vy;
-    },
+    };
 
-    drawOnCanvas : function(){
+    drawOnCanvas(){
         let ctx = this.canvas.getContext("2d");
-        ctx.clearRect(0,0, this.canvas.width, this.canvas.height)
+        //ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
         ctx.drawImage(this.img, this.x, this.y, 50, 50);
-    },
+    };
 
-    colisionsWithBorders : function(){
+    colisionsWithBorders(){
         if (this.y+50>this.canvas.height || this.y<0){
             this.vy=-this.vy;
         } 
         if (this.x+50>this.canvas.width || this.x<0){
             this.vx=-this.vx;
         }
-    },
+    };
 }
-
-
 let canvas = document.getElementById("paintCanvas");
+var ball, ball2;
 
 function updateState(){
     ball.move();
+    ball2.move();
 }
 
 function colisionCheck(){
     ball.colisionsWithBorders();
+    ball2.colisionsWithBorders();
 }
 
 function draw(){
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0,0, canvas.width, canvas.height);
     ball.drawOnCanvas();
+    ball2.drawOnCanvas();
 }
 
 function animationLoop(){
@@ -58,7 +62,9 @@ function animationLoop(){
 function startAnimation(){
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
-    ball.init(canvas);
+    ball = new Ball(canvas ,30,40);
+    ball2 = new Ball(canvas, 80,100);
+    ball2.vx=-ball2.vx;
     animationLoop();
 }
 
